19389c13d8e8ec6bb586c98e8d960afd63ce7e1a
Wilson Lourenço <49756885+wslourenco@users.noreply.github.com>
2026-03-08 14:17:45 -0300

    Initial commit

---
+++ src/main/java/com/curso/boot/DemoMvcAlunos202601Application.java
@@ -0,0 +1,13 @@
+package com.curso.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoMvcAlunos202601Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoMvcAlunos202601Application.class, args);
+	}
+
+}
